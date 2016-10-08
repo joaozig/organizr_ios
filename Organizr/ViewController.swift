59835b389e03084abbefadccf5a59b4e5018e153
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-08 12:51:54 -0300

    Initial Commit

---
+++ Organizr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Organizr
+//
+//  Created by João Ricardo Bastos on 08/10/16.
+//  Copyright © 2016 João Ricardo Bastos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
