6ab7b9290bbdc87418d4d5934cc6cf2b3c36edd4
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-08 15:29:57 -0300

    Adds Login Scene to Storyboard

---
+++ Organizr/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Organizr
+//
+//  Created by João Ricardo Bastos on 08/10/16.
+//  Copyright © 2016 João Ricardo Bastos. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
