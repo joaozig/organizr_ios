428abad4874125d294fadab23491428e257a57c1
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-10-27 19:13:02 -0300

    Adds getRequest method to HttpService

--- Organizr/HttpService.swift
+++ Organizr/HttpService.swift
@@ -7,8 +7,25 @@
 //
 
 import Foundation
+import KeychainSwift
 
 class HttpService {
+
+    func getRequest(url: String, callback: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        let url = NSURL(string: url)
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "GET"
+        let keychain = KeychainSwift()
+        let auth_token = keychain.get("username")
+        request.addValue(auth_token!, forHTTPHeaderField: "Authentication")
+        request.addValue("application/json", forHTTPHeaderField: "Content-type")
+
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+            callback(data, response, error)
+        }
+
+        task.resume()
+    }
 
     func postRequest(url: String, params: NSData?, callback: (NSData?, NSURLResponse?, NSError?) -> Void) {
         let url = NSURL(string: url)
